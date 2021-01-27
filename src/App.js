2dc1edd783556eea0ce29c9cbf9f881d04b23f8b
jieyingli <peter0512lee@gmail.com>
2021-01-27 15:08:33 +0800

    20210127

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import arrow from './material-icons-black-arrow-back.svg';
 import './App.css';
+import EmailInput from './components/EmailInput';
+import PasswordInput from './components/PasswordInput';
+import CheckPassword from './components/CheckPassword';
+import ButtonSubmit from './components/ButtonSubmit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={arrow} className="MaterialIcons-blackArrow-back" />
+      <div className="title">註冊帳號</div>
+      <EmailInput />
+      <div className="error-info">錯誤資訊</div>
+      <PasswordInput />
+      <div className="password-info">密碼必須包含8個字元以上</div>
+      <CheckPassword />
+      <div className="privacy-info">使用這個應用程式前，請先詳閱「Go Hiking」的<br />
+        《<span className="text-style-1">隱私權政策</span>》及《<span className="text-style-1">服務條款</span>》
+      </div>
+      <ButtonSubmit />
     </div>
   );
 }
---
+++ src/components/ButtonSubmit.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ButtonSubmit = () => (
+    <input className="Rectangle" type="button" defaultValue="同意並註冊" />
+);
+
+
+export default ButtonSubmit;
