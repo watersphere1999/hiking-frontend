82fd6e24a5829cceda03d85d43bee7a00cb8106b
Restyled.io <commits@restyled.io>
2021-01-28 08:20:33 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 import './App.css';
+
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Home from './pages/Home.js'
 import Register0 from './pages/Register0';
 import Register1 from './pages/Register1';
-import Home from './pages/Home.js'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
+  return (<Router><div><Switch><Route exact path = "/"><Home />
           </Route>
-          <Route path="/register">
-            <Register0 />
-          </Route>
-          <Route path="/register1">
-            <Register1 />
-          </Route>
+          <Route path="/register ">
+           < Register0 /></Route>
+          <Route path="/register1 ">
+                          < Register1 /></Route>
         </Switch>
-      </div>
-    </Router>
-  );
+          </div>
+    </Router>);
 }
 
 export default App;
