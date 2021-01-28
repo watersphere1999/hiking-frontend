7324f22d0b8d692c22ed68a8b5010b2da186d910
Ching-Hang Hsu <jerry1202.wtmh@gmail.com>
2021-01-28 14:11:33 +0800

    Home.js + Home.css + sample.jpg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import './Home.css';
+import sample from './sample.jpg';
 
-function Home() {
-    return (
-        <div>
-        <h2>Home</h2>
-        </div>
-    );
+function App() {
+  return (
+    <div className = "container">
+      <div className = "up">
+        <img src={sample} className = "sampleImage"></img>
+        <div className = "goHikingText">GO HIKING</div>
+      </div>
+      <div className = "down">
+        <input type = "button" className = "rectangleGoogle" defaultValue= "透過Google登入" />
+        <input type = "button" className = "rectangleFacebook" defaultValue= "透過Facebook登入" />
+        <input type = "button" className = "rectangleApple" defaultValue= "透過Apple登入" />
+        <input type = "button" className = "rectangleEmail" defaultValue= "透過Email登入" />
+      </div>
+      <div style = {{textAlign: 'center'}}>還不是會員嗎?<span>註冊新帳號</span></div>
+      <button type="button" className = "useButton">直接使用</button>
+    </div>
+  );
 }
 
-export default Home;
+export default App;
